565cc1e71ef3ca23f7577cf7a2d1723129133367
originalang <angeorge200@gmail.com>
2019-04-19 18:53:03 -0400

    Created the cli app, added commands to work with projects

---
+++ main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"sort"
+	"text/tabwriter"
+
+	"github.com/originalang/togoist"
+	"github.com/urfave/cli"
+)
+
+func main() {
+	app := cli.NewApp()
+
+	app.Commands = []cli.Command{
+		{
+			Name: "project",
+			Subcommands: []cli.Command{
+				{
+					Name:  "list",
+					Usage: "list projects",
+					Action: func(c *cli.Context) error {
+						dat, _ := ioutil.ReadFile("config")
+						client := togoist.NewClient(string(dat))
+						client.Sync()
+
+						w := new(tabwriter.Writer)
+						w.Init(os.Stdout, 8, 8, 0, '\t', 0)
+						defer w.Flush()
+
+						fmt.Fprintf(w, "\n %s\t%s\t%s\t%s\t", "Id", "Name", "Indent", "Favorite")
+						fmt.Fprintf(w, "\n %s\t%s\t%s\t%s\t", "--", "----", "------", "--------")
+
+						for _, proj := range client.Projects {
+							fmt.Fprintf(w, "\n %v\t%s\t%v\t%v\t", proj.Id, proj.Name, proj.Indent, proj.IsFavorite)
+						}
+
+						return nil
+					},
+				},
+
+				{
+					Name:  "add",
+					Usage: "add a new project",
+					Action: func(c *cli.Context) error {
+						dat, _ := ioutil.ReadFile("config")
+						client := togoist.NewClient(string(dat))
+						client.Sync()
+						client.AddProject(c.String("name"), c.Int("indent"))
+
+						return nil
+					},
+					Flags: []cli.Flag{
+						cli.StringFlag{
+							Name:  "name, n",
+							Value: "New Project",
+						},
+						cli.IntFlag{
+							Name:  "indent, i",
+							Value: 1,
+						},
+					},
+				},
+
+				{
+					Name:  "delete",
+					Usage: "delete a project",
+					Action: func(c *cli.Context) error {
+						dat, _ := ioutil.ReadFile("config")
+						client := togoist.NewClient(string(dat))
+						client.Sync()
+
+						ids := []int64{c.Int64("id")}
+						client.DeleteProjects(ids)
+
+						return nil
+					},
+					Flags: []cli.Flag{
+						cli.Int64Flag{
+							Name: "id",
+						},
+					},
+				},
+			},
+		},
+	}
+
+	sort.Sort(cli.FlagsByName(app.Flags))
+	sort.Sort(cli.CommandsByName(app.Commands))
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
